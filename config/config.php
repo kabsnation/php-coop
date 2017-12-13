5b55059b8c79c9ff8f230a6c965acee6ba334459
kabsnation <cpolidan123@gmail.com>
2017-12-13 15:15:07 +0800

    project
    first solution

---
+++ config/config.php
@@ -0,0 +1,41 @@
+<?php
+class Connect 
+{
+	private $db_host = 'localhost';
+	private $db_user ='root';
+	private $db_pass='1234';
+	private $db_database ='creotec';
+	private $conn = '';
+
+	public function __construct(){
+		$this->conn = $this -> connectDB();
+	}
+
+	public function connectDB(){
+		$conn = mysqli_connect($this->db_host,$this->db_user,$this->db_pass,$this->db_database) or die("Unable to connect to server");
+		return $conn;
+	}
+
+	public function insert($query){
+		$result = mysqli_query($this->conn, $query);
+		return $result;
+	}
+	public function insertWithLastId($query){
+		if(mysqli_query($this->conn, $query)){
+			return mysqli_insert_id($this->conn);
+		}
+		else{
+			return "";
+		}
+	}
+	public function insertWithLastId1($query){
+		$result = mysqli_query($this->conn, $query);
+		return $result;
+	}
+
+	public function select($query){
+		$result = mysqli_query($this->conn, $query);
+		return $result;
+	}
+}
+?>
